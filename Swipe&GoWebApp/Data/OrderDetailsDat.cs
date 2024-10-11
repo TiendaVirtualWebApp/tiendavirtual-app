6cca328934194d0618811357108ddad1ec9b4080
Carolina Alonso <carolinaalonsog34@gmail.com>
2024-10-10 21:40:08 -0500

    Se crea la clase detalles pedido con los cuatro metodos

---
+++ Swipe&GoWebApp/Data/OrderDetailsDat.cs
@@ -0,0 +1,121 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class OrderDetailsDat
+    {
+        Persistence objPer = new Persistence();
+
+        // Método para mostrar todos los detalles de pedidos
+        public DataSet showDetallesPedidos()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procSelectDetallesPedidos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        // Método para guardar un nuevo detalle de pedido
+        public bool saveDetallePedido(int _cantidad, decimal _precio, int _fkproducto, int _fkpedido)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procInsertDetallePedidos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_cantidad", MySqlDbType.Int32).Value = _cantidad;
+            objSelectCmd.Parameters.Add("v_precio", MySqlDbType.Decimal).Value = _precio;
+            objSelectCmd.Parameters.Add("v_producto_id", MySqlDbType.Int32).Value = _fkproducto;
+            objSelectCmd.Parameters.Add("v_pedido_id", MySqlDbType.Int32).Value = _fkpedido;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para actualizar un detalle de pedido
+        public bool updateDetallePedido(int _id, int _cantidad, decimal _precio, int _fkproducto, int _fkpedido)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procUpdateDetallePedidos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("v_cantidad", MySqlDbType.Int32).Value = _cantidad;
+            objSelectCmd.Parameters.Add("v_precio", MySqlDbType.Decimal).Value = _precio;
+            objSelectCmd.Parameters.Add("v_producto_id", MySqlDbType.Int32).Value = _fkproducto;
+            objSelectCmd.Parameters.Add("v_pedido_id", MySqlDbType.Int32).Value = _fkpedido;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para eliminar un detalle de pedido
+        public bool deleteDetallePedido(int _id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procDeleteDetallePedidos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_id", MySqlDbType.Int32).Value = _id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
