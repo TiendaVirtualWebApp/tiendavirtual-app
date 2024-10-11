19abcbd7516d30c6d7e90aad1ed1d7129c67116f
Dennis Velez <denisestivenvelez@gmail.com>
2024-10-11 11:38:38 -0500

    Se crea la clase productos con los cuatro metodos

---
+++ Swipe&GoWebApp/Data/ProductsDat.cs
@@ -0,0 +1,127 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+
+    public class ProductsDat
+    {
+        Persistence objPer = new Persistence();
+
+        // Método para mostrar todos los comentarios
+        public DataSet showProductos()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procSelectProductos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        // Método para guardar un nuevo comentario
+
+        public bool saveProducto(string _nombre, string _descripcion, decimal _precio, int _fkcategoria, int _fkproveedor)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procInsertProductos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_nombre", MySqlDbType.VarChar).Value = _nombre;
+            objSelectCmd.Parameters.Add("v_descripcion", MySqlDbType.Text).Value = _descripcion;
+            objSelectCmd.Parameters.Add("v_precio", MySqlDbType.Decimal).Value = _precio;
+            objSelectCmd.Parameters.Add("v_categoria_id", MySqlDbType.Int32).Value = _fkcategoria;
+            objSelectCmd.Parameters.Add("v_proveedor_id", MySqlDbType.Int32).Value = _fkproveedor;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+
+        // Método para actualizar un comentario
+
+        public bool updateProducto(int _id, string _nombre, string _descripcion, decimal _precio, int _fkcategoria, int _fkproveedor)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procUpdateProductos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("v_nombre", MySqlDbType.VarChar).Value = _nombre;
+            objSelectCmd.Parameters.Add("v_descripcion", MySqlDbType.Text).Value = _descripcion;
+            objSelectCmd.Parameters.Add("v_precio", MySqlDbType.Decimal).Value = _precio;
+            objSelectCmd.Parameters.Add("v_categoria_id", MySqlDbType.Int32).Value = _fkcategoria;
+            objSelectCmd.Parameters.Add("v_proveedor_id", MySqlDbType.Int32).Value = _fkproveedor;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        // Método para eliminar un comentario
+        public bool deleteProducto(int _id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procDeleteProductos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_id", MySqlDbType.Int32).Value = _id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
