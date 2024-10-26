e59be560868ab84630646ca7d040d5e68cd1a5f7
Dennis Velez <denisestivenvelez@gmail.com>
2024-10-26 18:37:33 -0500

    Se agrega el metodo DLL en la capa de Datos y se crea la clase productos en la capa logica

---
+++ Swipe&GoWebApp/Logic/ProductsLog.cs
@@ -0,0 +1,43 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web;
+
+namespace Logic
+{
+    public class ProductsLog
+    {
+        ProductsDat objProd = new ProductsDat();
+
+        // Método para mostrar todos los comentarios
+        public DataSet showProductos()
+        {
+            return objProd.showProductos();
+        }
+        //Metodo para mostrar unicamente el id y la descripcion de los Provedores, en el DropDownList
+        public DataSet showProductosDDL()
+        {
+            return objProd.showProductosDDL();
+        }
+        // Método para guardar un nuevo comentario
+        public bool saveProducto(string _nombre, string _descripcion, decimal _precio, int _fkcategoria, int _fkproveedor)
+        {
+            return objProd.saveProducto(_nombre, _descripcion, _precio, _fkcategoria,_fkproveedor);
+        }
+        // Método para actualizar un comentario
+
+        public bool updateProducto(int _id, string _nombre, string _descripcion, decimal _precio, int _fkcategoria, int _fkproveedor)
+        {
+            return objProd.updateProducto(_id, _nombre, _descripcion,_precio, _fkcategoria, _fkproveedor);
+        }
+        // Método para eliminar un comentario
+        public bool deleteProducto(int _id)
+        {
+            return objProd.deleteProducto(_id);
+        }
+    }
+}
