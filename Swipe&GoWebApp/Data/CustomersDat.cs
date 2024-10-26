afc2c2bf271b3c7e2e34a717cfcb94e6301b26f2
Dennis Velez <denisestivenvelez@gmail.com>
2024-10-26 18:47:26 -0500

    Se agrega el metodo DLL en la capa de Datos y se crea la clase cliente en la capa logica

--- Swipe&GoWebApp/Data/CustomersDat.cs
+++ Swipe&GoWebApp/Data/CustomersDat.cs
@@ -28,6 +28,22 @@ namespace Data
             return objData;
         }
 
+        //Metodo para mostrar DDL
+        public DataSet showClientesDDL()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procSelectClientesDDL";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
         // Método para guardar un nuevo cliente
         public bool saveCliente(string _nombre, string _apellido, string _direccion, string _telefono)
         {
---
+++ Swipe&GoWebApp/Logic/CustomersLog.cs
@@ -0,0 +1,42 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web;
+
+namespace Logic
+{
+    public class CustomersLog
+    {
+        CustomersDat objCust = new CustomersDat();
+
+        // Método para mostrar todos los clientes
+        public DataSet showClientes()
+        {
+            return objCust.showClientes();
+        }
+        //Metodo para mostrar DDL
+        public DataSet showClientesDDL()
+        {
+            return objCust.showClientesDDL();
+        }
+        // Método para guardar un nuevo cliente
+        public bool saveCliente(string _nombre, string _apellido, string _direccion, string _telefono)
+        {
+            return objCust.saveCliente(_nombre, _apellido,_direccion, _telefono);
+        }
+        // Método para actualizar un cliente
+        public bool updateCliente(int _id, string _nombre, string _apellido, string _direccion, string _telefono)
+        {
+            return objCust.updateCliente(_id, _nombre, _apellido, _direccion, _telefono);
+        }
+        // Método para eliminar un cliente
+        public bool deleteCliente(int _id)
+        {
+            return objCust.deleteCliente(_id);
+        }
+    }
+}
