a232fe38e5cea90dd46b7410501baa99c5d0555e
Dennis Velez <denisestivenvelez@gmail.com>
2024-11-10 19:27:42 -0500

    Se crea el formulario de la clase Usuarios

---
+++ Swipe&GoWebApp/Presentation/WFUsers.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFUsers.aspx.cs" Inherits="Presentation.WFUsers" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Gestionar Usuarios</h1>
+    <div>
+        <!-- Id de los usuarios -->
+        <asp:HiddenField ID="HFUsuariosId" runat="server" />
+        <!-- Nombre de los usuarios -->
+        <asp:Label ID="Label1" runat="server" Text="Nombre:"></asp:Label>
+        <asp:TextBox ID="TBNombre" runat="server"></asp:TextBox>
+        <br />
+        <!-- Apellido de los usuarios -->
+        <asp:Label ID="Label2" runat="server" Text="Apellido:"></asp:Label>
+        <asp:TextBox ID="TBApellido" runat="server"></asp:TextBox>
+        <br />
+        <!-- Correo de los usuarios -->
+        <asp:Label ID="Label3" runat="server" Text="Correo:"></asp:Label>
+        <asp:TextBox ID="TBCorreo" runat="server"></asp:TextBox>
+        <br />
+        <!-- Contraseña de los usuarios -->
+        <asp:Label ID="Label4" runat="server" Text="Contraseña:"></asp:Label>
+        <asp:TextBox ID="TBContrasena" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <!-- Direccion de los usuarios -->
+        <asp:Label ID="Label5" runat="server" Text="Direccion:"></asp:Label>
+        <asp:TextBox ID="TBDireccion" runat="server"></asp:TextBox>
+        <br />
+        <!-- Telefono de los usuarios -->
+        <asp:Label ID="Label6" runat="server" Text="Telefono:"></asp:Label>
+        <asp:TextBox ID="TBTelefono" runat="server"></asp:TextBox>
+        <br />
+        <!-- Tipo de los usuarios -->
+        <asp:Label ID="Label7" runat="server" Text="Tipo:"></asp:Label>
+        <asp:TextBox ID="TBTipo" runat="server"></asp:TextBox>
+        <br />
+        <!-- Salt de los usuarios -->
+        <asp:Label ID="Label8" runat="server" Text="Salt:"></asp:Label>
+        <asp:TextBox ID="TBSalt" runat="server"></asp:TextBox>
+        <br />
+
+
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+        <asp:Label ID="LblMsj" runat="server" Text="" ForeColor="Red"></asp:Label>
+        <br />
+
+        <asp:GridView ID="GVUsuarios" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GVUsuarios_SelectedIndexChanged" OnRowDeleting="GVUsuarios_RowDeleting" DataKeyNames="usu_id">
+            <Columns>
+                <asp:BoundField DataField="usu_id" HeaderText="Id" />
+                <asp:BoundField DataField="usu_nombre" HeaderText="Nombre" />
+                <asp:BoundField DataField="usu_apellido" HeaderText="Apellido" />
+                <asp:BoundField DataField="usu_correo" HeaderText="Correo" />
+                <asp:BoundField DataField="usu_contrasena" HeaderText="Contrasena" />
+                <asp:BoundField DataField="usu_direccion" HeaderText="Direccion" />
+                <asp:BoundField DataField="usu_telefono" HeaderText="Telefono" />
+                <asp:BoundField DataField="usu_tipo" HeaderText="Tipo" />
+                <asp:BoundField DataField="usu_salt" HeaderText="Salt" />
+
+
+                <asp:CommandField ShowSelectButton="True" ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+
+    </div>
+</asp:Content>
---
+++ Swipe&GoWebApp/Presentation/WFUsers.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Logic;
+
+namespace Presentation
+{
+    public partial class WFUsers : System.Web.UI.Page
+    {
+        // Instancia de la clase UsersDat para interactuar con la lógica
+        UsersLog objUser = new UsersLog();
+
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private string _correo;
+        private string _contrasena;
+        private string _direccion;
+        private string _telefono;
+        private string _tipo;
+        private string _salt;
+        // Bandera para saber si la operación fue exitosa
+        private bool executed = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Solo se ejecuta cuando se carga la página por primera vez (no en postbacks)
+            if (!Page.IsPostBack)
+            {
+                showUsuarios(); // Mostrar todos los usuarios
+            }
+        }
+
+        // Mostrar todos los usuarios en el GridView
+        private void showUsuarios()
+        {
+            DataSet objData = new DataSet();
+            objData = objUser.showUsuarios(); // Obtiene todos los usuarios
+            GVUsuarios.DataSource = objData; // Asigna el DataSet al GridView
+            GVUsuarios.DataBind(); // Enlaza los datos con el GridView
+        }
+
+        // Método para limpiar los TextBox 
+        private void Clear()
+        {
+            TBNombre.Text = "";
+            TBApellido.Text = "";
+            TBCorreo.Text = "";
+            TBContrasena.Text = "";
+            TBDireccion.Text = "";
+            TBTelefono.Text = "";
+            TBTipo.Text = "";
+            TBSalt.Text = "";
+            HFUsuariosId.Value = "";
+        }
+
+        // Guardar un nuevo usuario
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            // Capturar los datos del usuario
+            _nombre = TBNombre.Text;
+            _apellido = TBApellido.Text;
+            _correo = TBCorreo.Text;
+            _contrasena = TBContrasena.Text;
+            _direccion = TBDireccion.Text;
+            _telefono = TBTelefono.Text;
+            _tipo = TBTipo.Text;
+            _salt = TBSalt.Text;
+
+            // Llamada a la lógica para guardar el usuario
+            executed = objUser.saveUsuario(_nombre, _apellido, _correo, _contrasena, _direccion, _telefono, _tipo, _salt);
+
+            if (executed)
+            {
+                LblMsj.Text = "¡Usuario guardado exitosamente!";
+                LblMsj.ForeColor = System.Drawing.Color.Green;
+                Clear(); // Limpiar los TextBox después de guardar
+                showUsuarios(); // Mostrar los usuarios actualizados
+            }
+            else
+            {
+                LblMsj.Text = "¡Error al guardar el usuario!";
+                LblMsj.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        // Actualizar un usuario existente
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            // Obtener los datos del usuario
+            _id = Convert.ToInt32(HFUsuariosId.Value);
+            _nombre = TBNombre.Text;
+            _apellido = TBApellido.Text;
+            _correo = TBCorreo.Text;
+            _contrasena = TBContrasena.Text;
+            _direccion = TBDireccion.Text;
+            _telefono = TBTelefono.Text;
+            _tipo = TBTipo.Text;
+            _salt = TBSalt.Text;
+
+            // Llamada a la lógica de negocio para actualizar el usuario
+            executed = objUser.updateUsuario(_id, _nombre, _apellido, _correo, _contrasena, _direccion, _telefono, _tipo, _salt);
+
+            if (executed)
+            {
+                LblMsj.Text = "¡Usuario actualizado exitosamente!";
+                LblMsj.ForeColor = System.Drawing.Color.Green;
+                Clear();
+                showUsuarios(); // Mostrar los usuarios actualizados
+            }
+            else
+            {
+                LblMsj.Text = "¡Error al actualizar el usuario!";
+                LblMsj.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        // Evento para seleccionar una fila en el GridView y cargar los datos en los controles
+        protected void GVUsuarios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Obtener el ID del usuario seleccionado
+            HFUsuariosId.Value = GVUsuarios.SelectedRow.Cells[0].Text;
+            TBNombre.Text = GVUsuarios.SelectedRow.Cells[1].Text;
+            TBApellido.Text = GVUsuarios.SelectedRow.Cells[2].Text;
+            TBCorreo.Text = GVUsuarios.SelectedRow.Cells[3].Text;
+            TBContrasena.Text = GVUsuarios.SelectedRow.Cells[4].Text;
+            TBDireccion.Text = GVUsuarios.SelectedRow.Cells[5].Text;
+            TBTelefono.Text = GVUsuarios.SelectedRow.Cells[6].Text;
+            TBTipo.Text = GVUsuarios.SelectedRow.Cells[7].Text;
+            TBSalt.Text = GVUsuarios.SelectedRow.Cells[8].Text;
+        }
+
+        // Evento para eliminar un usuario
+        protected void GVUsuarios_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int userId = Convert.ToInt32(GVUsuarios.DataKeys[e.RowIndex].Values[0]);
+            executed = objUser.deleteUsuario(userId);
+
+            if (executed)
+            {
+                LblMsj.Text = "El usuario se eliminó exitosamente";
+                GVUsuarios.EditIndex = -1;
+                LblMsj.ForeColor = System.Drawing.Color.Green;
+                showUsuarios();
+            }
+            else
+            {
+                LblMsj.Text = "Error al eliminar el usuario";
+                LblMsj.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
