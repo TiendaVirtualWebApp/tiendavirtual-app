761c7d034730d5f5b5568ea3f77663f85235f6d6
CarolinaAlonso34 <carolinaalonsog34@gmail.com>
2024-11-13 17:00:14 -0500

    Se crea el formulario de la tabla Pagos

--- Swipe&GoWebApp/Presentation/WFPayments.aspx.cs
+++ Swipe&GoWebApp/Presentation/WFPayments.aspx.cs
@@ -71,7 +71,7 @@ namespace Presentation
         {
             try
             {
-                _fecha = Convert.ToDateTime(TBFecha.Text);
+                _fecha = DateTime.ParseExact(TBFecha.Text, "MM-dd-yyyy HH:mm:ss tt", CultureInfo.InvariantCulture);
                 _monto = Convert.ToDouble(TBMonto.Text);
                 _metodoPago = TBMetodoPago.Text;
                 _estado = TBEstatus.Text;
@@ -105,7 +105,7 @@ namespace Presentation
             try
             {
                 _id = Convert.ToInt32(HFPagoId.Value);
-                _fecha = Convert.ToDateTime(TBFecha.Text);
+                _fecha = DateTime.ParseExact(TBFecha.Text, "MM-dd-yyyy HH:mm:ss tt", CultureInfo.InvariantCulture);
                 _monto = Convert.ToDouble(TBMonto.Text);
                 _metodoPago = TBMetodoPago.Text;
                 _estado = TBEstatus.Text;
